18a3dc71d40e882ed86f227a355183498cd30342
Alexander Kalashnikov <aguglovec@gmail.com>
2022-04-06 12:28:56 +0300

    DZ05

---
+++ DZ05/scripts.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const students = [
+    {
+        id:10,
+        name: 'John Smith',
+        marks: [10, 8, 6, 9, 8, 7 ]
+    },
+    {
+        id:11,
+        name: 'John Doe',
+        marks: [ 9, 8, 7, 6, 7 ]
+    },
+    {
+        id:12,
+        name: 'Thomas Anderson',
+        marks: [6, 7, 10, 8 ]
+    },
+    {
+        id:13,
+        name: 'Jean-Baptiste Emanuel Zorg',
+        marks: [10, 9, 8, 9 ]
+    }
+]
+
+averageStudentMark(students[2]);
+averageGroupMark(students);
+
+function averageStudentMark (studentIndex) {
+return avarageMark(studentIndex.marks)
+}
+
+function averageGroupMark (group) {
+    // let allMarks = group.reduce ((acc,currentStudent) => [...acc, ...currentStudent.marks], []);
+    let allMarks = group.reduce ((acc,currentStudent) => acc.concat(currentStudent.marks), []);
+    return avarageMark (allMarks)
+}
+
+function avarageMark (arr) {
+return arr.reduce ((acc,currentValue) => acc += currentValue,) / arr.length
+}
