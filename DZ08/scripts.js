04533410181c5267a9d2139a64cdd71ac7d830f2
aguglovec@gmail.com <aguglovec@gmail.com>
2022-04-17 22:14:42 +0300

    errMsg

--- DZ08/scripts.js
+++ DZ08/scripts.js
@@ -53,7 +53,7 @@ function isValid () {
 
 function errorMsg(whereError) {
     if (whereError) {
-      errorMsgEl.textContent = `Wrond characters are entered in ${whereError}. Only 0,1,2,3,4,5,6,7,8,9,"." are allowed!`;
+      errorMsgEl.textContent = `Wrong characters are entered in ${whereError}. Only 0,1,2,3,4,5,6,7,8,9,"." are allowed!`;
     } else {
       errorMsgEl.textContent = ``;
     }
