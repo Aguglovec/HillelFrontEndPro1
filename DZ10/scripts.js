ced435282ea00254ba5528b0778e55f98300d927
Alexander Kalashnikov <aguglovec@gmail.com>
2022-04-28 16:52:26 +0300

    DZ10

--- DZ10/scripts.js
+++ DZ10/scripts.js
@@ -1,66 +1,85 @@
 "use strict";
 
-const newTaskInput = document.getElementById("newTask");
+
+const CONTACT_ITEM_TEMPLATE = document.getElementById('contactItemTemplate').innerHTML;
+const ERROR_MESSAGES = {
+    NAME: 'Enter Name',
+    SURNAME: 'Enter Surname',
+    PHONE: 'Enter Phone number'
+};
+
+
+const nameInput = document.getElementById("newName");
+const surnameInput = document.getElementById("newSurname");
+const numberInput = document.getElementById("newNumber");
 const errorMsgEl = document.getElementById("error"); 
 const addBtnEl = document.getElementById("addBtn");
-const taskListEL = document.getElementById("toDoList");
+const contactListEL = document.getElementById("contactList");
+const newContactFormEL = document.getElementById("newContact");
+
 
 addBtnEl.addEventListener("click", onAddBtnClick);
-newTaskInput.addEventListener("input", isValid);
+// newTaskInput.addEventListener("input", isValid);
 
 // Захотел чтобы по нажатию на Enter добавлялась новая задача в список ToDo.
-newTaskInput.addEventListener("keyup", enterPress);
+// newTaskInput.addEventListener("keyup", enterPress);
 
-function enterPress (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault(); //Без этой штуки тоже работает. Зачем она?
-        onAddBtnClick();
-    }
-};
+// function enterPress (event) {
+//     if (event.key === 'Enter') {
+//         event.preventDefault(); //Без этой штуки тоже работает. Зачем она?
+//         onAddBtnClick();
+//     }
+// };
 
 function onAddBtnClick() {
     if (isValid()) {   
-        addTask();
+        addContact();
         clearInput();
     }
 }
 
+//Вопрос: как лучше проверять на пустой input? .value.length < 1 ИЛИ .value === ''?
 function isValid () {
-    if (newTaskInput.value.length < 1)  {
-        errorMsg('Enter something');
+    if (nameInput.value.length < 1)  {
+        errorMsg(ERROR_MESSAGES.NAME);
         return false;
     }  
-    if (newTaskInput.value.length < 4) {
-        errorMsg('Task is too short');
+    if (surnameInput.value.length < 1)  {
+        errorMsg(ERROR_MESSAGES.SURNAME);
         return false;
-    } 
+    }
+    if (numberInput.value.length < 1)  {
+        errorMsg(ERROR_MESSAGES.PHONE);
+        return false;
+    }
     errorMsg();
     return true;
 }
 
-function addTask() {
-    const li = createLi();
-    taskListEL.append(li);
-    li.addEventListener("click", taskStatus);
+function addContact() {
+    const contact = createContactItem();
+    newContactFormEL.insertAdjacentHTML('beforebegin', contact);
 }
+
+function createContactItem() {
+        return CONTACT_ITEM_TEMPLATE
+            .replace('{{name}}', nameInput.value)
+            .replace('{{surname}}', surnameInput.value)
+            .replace('{{number}}', numberInput.value);
+}
+
+// function taskStatus () {
+//     this.classList.toggle('taskDone');
+
+// }
 
 function clearInput() {
-    newTaskInput.value = ''
-}
-
-function createLi() {
-    const el = document.createElement('li');
-    el.classList.add('taskToDo');
-    el.textContent = newTaskInput.value;
-    return el;
-}
-
-function taskStatus () {
-    this.classList.toggle('taskDone');
-
+    nameInput.value = '';
+    surnameInput.value = '';
+    numberInput.value = '';
 }
 
 function errorMsg(error) {
     errorMsgEl.textContent = error;
-    (error) ? newTaskInput.classList.add('error') : newTaskInput.classList.remove('error');
+    // (error) ? newTaskInput.classList.add('error') : newTaskInput.classList.remove('error');
 }
