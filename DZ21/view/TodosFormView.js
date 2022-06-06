70f17eb135550c0193600337640075718ed36668
Alexander Kalashnikov <aguglovec@gmail.com>
2022-06-06 20:59:07 +0300

    DZ21

--- DZ21/view/TodosFormView.js
+++ DZ21/view/TodosFormView.js
@@ -1,33 +1,41 @@
 class TodosFormView {
 
 static FORM_TEMPLATE = `<form id="newTaskForm">
-    <input type="text" id="newTask" placeholder="Enter new task here" value="" autofocus name='title'>
+    <input type="text" class="form-input" id="title" placeholder="Enter new task here" value="" autofocus name='title'>
     <button id="addBtn">Add task</button>
     <div class="error" id="error"></div>
         </form>`;
 
 
 static FORM_SELECTOR = '#newTaskForm'
-static INPUT_SELECTOR = '#newTaskForm'
+static INPUT_SELECTOR = '#newTask'
+static FORM_INPUTS_SELECTOR = '.form-input'
 
 
         constructor(config = {}) {
             this.$el = $(TodosFormView.FORM_TEMPLATE)
-                .on('submit', TodosFormView.FORM_SELECTOR,
-                    (e) =>
-                        config.onAdd && config.onAdd($(INPUT_SELECTOR).val()),
-                )
-                // .on('input', TodosFormView.TASK_DELETE_SELECTOR, (e) => {
-                //     e.stopPropagation();
-                //     config.onDelete &&
-                //         config.onDelete(
-                //             $(e.target)
-                //                 .closest(TodosFormView.FORM_SELECTOR)
-                //                 .data(TodosFormView.TASK_DATA_ID),
-                //         );
-                // });
+                .on('submit', (e) => {
+                    e.preventDefault();
+                    console.log(e);
+                    config.onAdd(this.getItemFromForm());
+                });
         }
 
+        // submitInput(e) {
+        //     e.preventDefault();
+        //     console.log(e);
+        //     this.config.onAdd($(TodosFormView.INPUT_SELECTOR).val()); 
+        // }
+        
 
+        getItemFromForm() {
+            const item = {};
+        console.log($(TodosFormView.FORM_INPUTS_SELECTOR));
+            $(TodosFormView.FORM_INPUTS_SELECTOR).map((inp) => {
+                item[inp.name] = inp.value;
+            });
+        
+            return item;
 
+}
 }
