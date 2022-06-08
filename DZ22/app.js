00a8c937f3583c682bbb2281cc78fbf8302e53c7
Alexander Kalashnikov <aguglovec@gmail.com>
2022-06-08 22:14:38 +0300

    DZ22

--- DZ22/app.js
+++ DZ22/app.js
@@ -1,8 +1,14 @@
-const calc = require('./calculator');
+const calculator = require('./calculator');
 
-calc.set(10);
-calc.div(5);
-calc.mult(2);
-calc.sub(1);
-calc.add(2);
-calc.result();
+calculator.set(10); //10
+calculator.div(5);  //2
+calculator.mult(2); //4
+calculator.sub(1);  //3
+calculator.add(2);  //5
+calculator.result();    //5
+
+calculator.set(10);
+console.log(calculator.add(2)) // 12
+console.log(calculator.sub(5)) // 7
+console.log(calculator.mult(4)) // 28
+console.log(calculator.div(2)) // 14
