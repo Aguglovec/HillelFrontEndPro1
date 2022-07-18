1d88741c9ac680300f154e8bdb31e5fc9fc372dc
Alexander Kalashnikov <aguglovec@gmail.com>
2022-07-18 13:34:16 +0300

    DZ30

--- DZ30_UserList_React/src/components/ErrorMsg/ErrorMsg.js
+++ DZ30_UserList_React/src/components/ErrorMsg/ErrorMsg.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './ErrorMsg.css'
 
-export default class ErrorMsg extends Component {
+export default class ErrorMsg  extends Component {
     render() {
         return (
             <div className='error'>{this.props.error ? this.props.error : null}</div>
