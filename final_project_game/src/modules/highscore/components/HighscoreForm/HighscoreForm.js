ddcf1858afd081168846641320e44d93b034b5c1
Alexander Kalashnikov <aguglovec@gmail.com>
2022-08-17 10:39:09 +0300

    final

--- final_project_game/src/modules/highscore/components/HighscoreForm/HighscoreForm.js
+++ final_project_game/src/modules/highscore/components/HighscoreForm/HighscoreForm.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { resetWinState } from '../../../../store/actions/game25Actions';
 
 function HighscoreForm() {
+    const dispatch = useDispatch();
     const [name, setName] = useState("")
 
     function onHighscoreSubmit(e) {
         e.preventDefault();
+        dispatch(resetWinState());
         console.log(name);
     }
     
 return (
+    <>
     <form onClick={(e) => e.stopPropagation()} onSubmit={onHighscoreSubmit}>
-        <input value={name} onChange={(e) => setName(e.target.value)} />
-        <button>Submit</button>
+    <input type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} />
+    <button>Submit</button>
     </form>
+    </>
     )
 }
 
