281121e4a06eb44dc75f05aedef84337f122a167
Alexander Kalashnikov <aguglovec@gmail.com>
2022-08-20 14:23:49 +0300

    final

--- final_project_game/src/config.js
+++ final_project_game/src/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://62fa20ba3c4f110faa92a62a.mockapi.io/';
-export const START_DELAY = 5000;
+export const START_DELAY = 10000;
 export const DIFF_EASY = "easy";
 export const DIFF_REGULAR = "regular";
 export const DIFF_HARD = "hard";
